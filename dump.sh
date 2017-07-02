367825acb429febd8aa7374123de447654493d13
Brad Beattie <bradbeattie@gmail.com>
2017-07-01 21:55:17 -0700

    Tweaks

--- dump.sh
+++ dump.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mysqldump parliamentary_data $(mysql -D parliamentary_data -Bse "show tables WHERE Tables_in_parliamentary_data REGEXP '^(parliaments|elections|proceedings)_'") > deployed.sql
 rm deployed.sql.xz
-xz -9 deployed.sql
+xz deployed.sql
